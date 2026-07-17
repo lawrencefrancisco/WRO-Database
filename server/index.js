6d444fe038449891a6408eb22f8ffdd28a9ebdc8
LawLaw <lawrence.dhaniel@gmail.com>
2026-07-17 13:32:23 +0800

    50mb middleware

--- server/index.js
+++ server/index.js
@@ -16,9 +16,8 @@ app.use(cors({
   origin: process.env.CORS_ORIGIN || 'http://localhost',
   credentials: true,
 }));
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // ── Routes ────────────────────────────────────────────────────
 app.use('/api/auth',           require('./routes/auth'));
